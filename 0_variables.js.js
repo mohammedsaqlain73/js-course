b41e0d238efdb958d1c4829e24d488a4b62e8e81
mohammed saqlain <csesaq07@gmail.com>
2025-07-22 18:40:52 +0530

    first commit

---
+++ 0_variables.js.js
@@ -0,0 +1,28 @@
+//* ====================================
+//* Values and Variable in JavaScript
+//* ====================================
+
+//* In JavaScript, values and variables are fundamental concepts that form the basis of programming.
+
+//* Values:  A value is a piece of information that a program can work with. It can be a number, text, true/false, or more complex data.
+
+//* Variables: A variable is a container that holds a value. It has a name and can be used to store and manipulate data in a program.
+
+//* 1.varibales must start with a letter/underscore/dollar sign and have any special charecters.
+//* 2.variable names have spaces.
+//* 3.variables cannot be same as keywords.
+//* 4.variable names are case sensitive.
+//* 5.by convention VN are written in camelCase.
+
+//! Let's test
+//*var my_firstName = "saqlain";
+//*console.log(my_firstName); imp thing variable name for output not in quotes
+
+// var _myLastName$ = "Doe";
+
+// var 123myAge = 25; no quotes for numbers.invlais as cant start with no.
+
+// var $cityName = "New York";
+
+// var my@Email = "john@example.com"; this is invalid as in js only letter
+// underscore and dollar sign allowed no other special symbols.
